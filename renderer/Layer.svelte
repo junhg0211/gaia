2a33334445139922ba82ba8d04ba0d732ebae621
Sch <junhg0211@gmail.com>
2025-10-03 16:58:28 +0900

    add more buttons

--- renderer/Layer.svelte
+++ renderer/Layer.svelte
@@ -1,20 +1,23 @@
 <script>
   import Area from './Area.svelte';
   import Layer from './Layer.svelte';
-  import { createEventDispatcher } from 'svelte';
+  import { Quadtree, Area as AreaType, Layer as LayerType } from "../dataframe.js";
 
   export let layer;
 
-  const dispatch = createEventDispatcher();
-  function addArea() {
-    dispatch('addArea', { layerId: layer.id });
-  }
-  function addLayer() {
-    dispatch('addLayer', { parentId: layer.id });
+  let color = "#ff0000";
+  let name = "new area";
+
+  async function addArea() {
+    const newArea = new AreaType(null, color, layer, name);
+    layer.areas = [...layer.areas, newArea];
   }
 
-  function passThrough(event) {
-    dispatch(event.type, event.detail);
+  let layerName = "new layer";
+
+  async function addLayer() {
+    const newLayer = new LayerType(null, new Quadtree(), layer, [0,0], [1,1], layerName);
+    layer.children = [...layer.children, newLayer];
   }
 </script>
 
@@ -22,14 +25,21 @@
   <div>{layer.name}</div>
   <div>
     <button on:click={addArea}>영역 추가</button>
+    <div>
+      <input type="color" bind:value={color} />
+      <input type="text" bind:value={name} />
+    </div>
     {#each layer.areas as area}
       <Area {area} />
     {/each}
   </div>
   <div>
     <button on:click={addLayer}>레이어 추가</button>
+    <div>
+      <input type="text" bind:value={layerName} />
+    </div>
     {#each layer.children as child}
-    <Layer layer={child} on:addLayer={passThrough} on:addArea={passThrough} />
+    <Layer layer={child} />
     {/each}
   </div>
 </div>
